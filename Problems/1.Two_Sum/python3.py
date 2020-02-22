37a0af73b04052886072d95a5923d9d80e082b0e
Dot Cink <Dot.Cink+github@gmail.com>
2020-02-22 15:30:58 +0800

    [Problems/1.Two_Sum python3] small optimization 1. decrease more than 3 `len` to 1; 2. decrease 1 outer loop;

--- Problems/1.Two_Sum/python3.py
+++ Problems/1.Two_Sum/python3.py
@@ -1,10 +1,11 @@
 class Solution:
   def twoSum(self, nums: List[int], target: int) -> List[int]:
-    if nums == None or len(nums) < 2:
+    size = 0 if nums == None else len(nums)
+    if size < 2:
       return []
     
-    for i in range(len(nums)):
-      for j in range(i + 1, len(nums)):
+    for i in range(size - 1):
+      for j in range(i + 1, size):
         if nums[i] + nums[j] == target:
           return [i, j]
     return []
